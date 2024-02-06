a3b202af7e8a85a9585b922621d742c8c0b5f2d9
kitcurio <kasiarivers@gmail.com>
2024-02-05 20:34:01 -0500

    Initial Commit

---
+++ CraveFreer/CraveFreerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraveFreerApp.swift
+//  CraveFreer
+//
+//  Created by Kasia Rivers on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CraveFreerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
